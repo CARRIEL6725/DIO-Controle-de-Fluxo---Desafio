59fd857aa40836c64ce594861f0f9ddde8280018
CARRIEL6725 <rafael.carriel21@gmail.com>
2025-05-19 19:52:03 -0300

    Testando

--- Desafio/src/Contador.java
+++ Desafio/src/Contador.java
@@ -1,12 +1,9 @@
-import java.util.Scanner;
+
 
 public class Contador {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
 
-        System.out.println("diga o seu nome:");
-        String nome = sc.nextLine();
-
-        System.out.println("Ola," + nome);
+        System.out.println("Hello, World");
     }
+       
 }
