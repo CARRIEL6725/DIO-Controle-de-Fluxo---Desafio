84339139c74d3d8e2399cb4dd96eca89ea85fcf3
CARRIEL6725 <rafael.carriel21@gmail.com>
2025-05-20 17:12:32 -0300

    O código final

--- Desafio/src/Contador.java
+++ Desafio/src/Contador.java
@@ -1,9 +1,33 @@
-
+import java.util.Scanner;
 
 public class Contador {
-    public static void main(String[] args){
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Hello, World");
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = sc.nextInt();
+
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = sc.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            sc.close();
+        }
     }
-       
+
+    private static void contar(int parametroUm, int parametroDois){
+        if (parametroUm >= parametroDois) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
 }
